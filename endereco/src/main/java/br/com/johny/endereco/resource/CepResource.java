a5d4fce858c1ced90d08179f9a58f6f44a44db4f
Johny Alencar <johnyguido@hotmail.com.br>
2023-07-06 21:31:42 -0300

    criada integração com serviço de consulta de CEP

--- endereco/src/main/java/br/com/johny/endereco/resource/CepResource.java
+++ endereco/src/main/java/br/com/johny/endereco/resource/CepResource.java
@@ -3,6 +3,7 @@ package br.com.johny.endereco.resource;
 import br.com.johny.endereco.service.ViaCepService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class CepResource {
 
     private final ViaCepService viaCepService;
 
+    @GetMapping("/{cep}")
     public ResponseEntity<?> obterCep(@PathVariable ("cep") String cep){
         return ResponseEntity.ok(viaCepService.obterCep(cep));
     }
--- endereco/src/main/java/br/com/johny/endereco/service/ViaCepService.java
+++ endereco/src/main/java/br/com/johny/endereco/service/ViaCepService.java
@@ -2,11 +2,13 @@ package br.com.johny.endereco.service;
 
 import br.com.johny.endereco.response.EnderecoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
 
+    @GetMapping("/{cep}/json")
     EnderecoResponse obterCep(@PathVariable("cep") String cep);
 
 }
